dd9db9b36f0be5988e98dccd290236b2091ecb3b
meh <boldx@rucsok.hu>
2021-06-27 22:25:50 +0200

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,34 @@ lazy_static! {
     static ref BUFF: Spinlock<Vec<u8>> = Spinlock::new(Vec::<u8>::new());
 }
 
+fn left_pad(target_char_len: usize, pad_str: &str, str_to_pad: &str) -> String {
+    let pad_len = target_char_len.checked_sub(str_to_pad.chars().count()).unwrap_or(0);
+    let mut padding = String::new();
+    let mut pad_str_it = pad_str.chars().cycle();
+    while padding.chars().count() < pad_len {
+        match pad_str_it.next() {
+            Some(char) => padding.push(char),
+            None => break,
+        }
+    }
+    padding + str_to_pad
+}
+
+fn parse_args<'a>(arg_str: &'a str) -> Result<(usize, &'a str, &'a str), &'static str> {
+    let args = arg_str.splitn(3, " ").collect::<Vec<&str>>();
+    if args.len() != 3 {
+        return Err("Too few args")
+    }
+    let target_len = args[0].parse::<usize>().unwrap_or(0);
+    let mut pad_str = args[1];
+    let mut str_to_pad = args[2];
+    if pad_str.is_empty() {
+        pad_str = " ";
+        str_to_pad = &str_to_pad[1..];
+    }
+    Ok((target_len, pad_str, str_to_pad))
+}
+
 struct LeftPadFile;
 
 impl linux_kernel_module::file_operations::FileOperations for LeftPadFile {
@@ -26,10 +54,8 @@ impl linux_kernel_module::file_operations::FileOperations for LeftPadFile {
          buf: &mut linux_kernel_module::user_ptr::UserSlicePtrWriter,
          offset: u64|
          -> linux_kernel_module::KernelResult<()> {
-            if offset == 0 { 
-                //let val = unsafe { BUFF.clone() };
-                let val = (*BUFF.lock()).clone();
-                buf.write(&val)?;
+            if offset == 0 {
+                buf.write(&*(BUFF.lock()))?;
             }
             Ok(())
         },
@@ -41,37 +67,13 @@ impl linux_kernel_module::file_operations::FileOperations for LeftPadFile {
          _offset: u64|
          -> linux_kernel_module::KernelResult<()> {
             let data = buf.read_all()?;
-            
-            let params = String::from_utf8(data).unwrap_or(String::new());
-            let params = params.splitn(3, " ").collect::<Vec<&str>>();
-            if params.len() != 3 {
-                return Err(Error::EINVAL)
-            }
-            
-            let target_len = params[0].parse::<usize>().unwrap_or(0);
-            let mut pad_unit = params[1];
-            let mut str_to_pad = params[2];
-            if pad_unit.is_empty() {
-                pad_unit = " ";
-                str_to_pad = &str_to_pad[1..];
-            }
-            
-            let pad_len = target_len.checked_sub(str_to_pad.chars().count()).unwrap_or(0);
-            let mut pad_str = String::new();
-            let mut pad_unit_it = pad_unit.chars().cycle();
-            while pad_str.chars().count() < pad_len {
-                match pad_unit_it.next() {
-                    Some(char) => pad_str.push(char),
-                    None => break,
-                }
-            }
-            let padded_str = pad_str + str_to_pad;
-/*
-            unsafe { 
-                BUFF.clear();
-                BUFF.extend_from_slice(padded_str.as_bytes());
-            }; 
-*/          let mut buff = BUFF.lock();
+            let args = String::from_utf8(data).unwrap_or(String::new());
+            let (target_len, pad_unit, str_to_pad) = match parse_args(&args) {
+                Ok(t) => t,
+                Err(_) => return Err(Error::EINVAL),
+            };
+            let padded_str = left_pad(target_len, pad_unit, str_to_pad);
+            let mut buff = BUFF.lock();
             (*buff).clear();
             (*buff).extend_from_slice(padded_str.as_bytes());
             Ok(())
@@ -81,18 +83,16 @@ impl linux_kernel_module::file_operations::FileOperations for LeftPadFile {
 
 
 struct LeftPadModule {
-    _chrdev_registration: linux_kernel_module::chrdev::Registration,
+    _reg: linux_kernel_module::chrdev::Registration,
 }
 
 impl linux_kernel_module::KernelModule for LeftPadModule {
     fn init() -> linux_kernel_module::KernelResult<Self> {
-        let chrdev_registration =
+        let _reg =
             linux_kernel_module::chrdev::builder(cstr!("leftpad"), 0..1)?
                 .register_device::<LeftPadFile>()
                 .build()?;
-        Ok(LeftPadModule {
-            _chrdev_registration: chrdev_registration,
-        })
+        Ok(LeftPadModule { _reg })
     }
 }
 
@@ -103,3 +103,4 @@ linux_kernel_module::kernel_module!(
     description: b"A module for left-padding strings",
     license: b"GPL"
 );
+
